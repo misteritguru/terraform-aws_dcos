4184d42c6061487e4ad3923a771658cf7724113b
Christian Tragesser <christian@evoen.net>
2016-04-24 11:44:59 -0500

    adding ssh connection rule until figuring out terraform connection to private IP

--- security_groups.tf
+++ security_groups.tf
@@ -32,6 +32,13 @@ resource "aws_security_group" "mesos_server" {
   description = "Mesos agent to server communication"
 
   ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port = 80
     to_port = 80
     protocol = "tcp"
