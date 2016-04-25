6a84924e8e9684f0e7ed5dfb529870896bb55e66
Christian Tragesser <christian@evoen.net>
2016-04-24 20:37:23 -0500

    attempt to sed substitute master and agent IPs into config.yaml

--- dcos_hosts.tf
+++ dcos_hosts.tf
@@ -22,7 +22,11 @@ resource "aws_instance" "dcos_bootstrap" {
       "git clone https://github.com/christianTragesser/cookbook-ctt_dcos.git ~/cookbooks/ctt_dcos",
       "cd ~/cookbooks/ctt_dcos && berks install",
       "cd ~/cookbooks/ctt_dcos && berks vendor ~/cookbooks",
-      "sudo chef-client -z -o ctt_dcos::bootstrap"
+      "sudo chef-client -z -o ctt_dcos::bootstrap",
+      "sudo sed -i -e 's/MASTER0/${aws_instance.dcos_master0.private_ip}/' \
+       -e 's/AGENT0/${aws_instance.dcos_agent0.private_ip}/' \
+       -e 's/AGENT1/${aws_instance.dcos_agent1.private_ip}/' \
+       -e 's/AGENT2/${aws_instance.dcos_agent2.private_ip}/' /root/genconf/config.yaml"
       ]
     }
 }
