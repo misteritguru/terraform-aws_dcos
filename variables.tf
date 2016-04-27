37fa49becf6375589e172b591a075eb55897a8be
Christian Tragesser <christian@evoen.net>
2016-04-27 15:28:56 -0500

    add private ssh key as variable to auto create needed key

--- variables.tf
+++ variables.tf
@@ -2,6 +2,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "aws_key_name" {}
 variable "aws_key_path" {}
+variable "private_ssh_key" {}
 variable "vpc_id" {}
 variable "vpc_region" {}
 variable "az" {}
